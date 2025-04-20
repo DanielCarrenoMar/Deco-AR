4fd1a0e68b322c627943bd0a4ff45bd16c25d162
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-04-20 15:01:27 -0400

    feat: FModelDao terminado

--- app/src/main/java/com/app/homear/data/database/dao/FModelDao.kt
+++ app/src/main/java/com/app/homear/data/database/dao/FModelDao.kt
@@ -1,6 +1,8 @@
 package com.app.homear.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.app.homear.data.database.entity.FModelEntity
 
@@ -8,5 +10,23 @@ import com.app.homear.data.database.entity.FModelEntity
 interface FModelDao {
 
     @Query("SELECT * FROM fmodel")
-    suspend fun getAllCourses(): List<FModelEntity>
+    suspend fun getAllFModels(): List<FModelEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFModel(fModel: FModelEntity): Long
+
+    @Query("UPDATE fmodel SET name = :name, description = :description WHERE id = :fModelId")
+    suspend fun updateFModelById(fModelId: Int, name: String, description: String): Int
+
+    @Query("DELETE FROM fmodel")
+    suspend fun deleteAllFModels(): Int
+
+    @Query("DELETE FROM sqlite_sequence WHERE name = 'fmodel'")
+    suspend fun resetIncremetalFModel()
+
+    @Query("DELETE FROM fmodel WHERE id = :fModelId")
+    suspend fun deleteFModelFromId(fModelId: Int): Int
+
+    @Query("SELECT * FROM fmodel WHERE id = :fModelId")
+    suspend fun getModelFromId(fModelId: Int): FModelEntity?
 }
