8c73598f9ccb8bda86bd02377a8f381ba99d807a
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-04-20 15:24:47 -0400

    feat: interfas de repositorio para FModel

--- app/src/main/java/com/app/homear/domain/repository/LocalStorageRepository.kt
+++ app/src/main/java/com/app/homear/domain/repository/LocalStorageRepository.kt
@@ -1,5 +1,6 @@
 package com.app.homear.domain.repository
 
+import com.app.homear.domain.model.FModelModel
 import java.io.File
 
 interface LocalStorageRepository {
@@ -8,4 +9,9 @@ interface LocalStorageRepository {
      */
     suspend fun getAllFilesTypeFromDir(directory: String, type: String): List<File>
 
+    suspend fun getAllFModels(): List<FModelModel>
+    suspend fun insertFModel(fModel: FModelModel): Long
+    suspend fun updateFModelById(fModelId: Int, name: String, description: String): Int
+    suspend fun deleteAllFModels(): Int
+    suspend fun deleteFModelFromId(fModelId: Int): Int
 }
