bbe38ca83eab0a71faca41add973fa3b8fa69d5f
JPol0 <160655545+JPol0@users.noreply.github.com>
2025-05-25 23:12:09 -0400

    feat: Pantalla para visualizar opciones terminada
    del ultimo cambio se agredo, una animacion para reducir la escala de la opcion cuando se pulsa, el componente Opcion CheckBox, y por ultimo el componente Opcion DropMenu

--- app/src/main/java/com/app/homear/ui/configuracion/ScreenConfiguracion.kt
+++ app/src/main/java/com/app/homear/ui/configuracion/ScreenConfiguracion.kt
@@ -3,8 +3,15 @@ package com.app.homear.ui.configuracion
 
 
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
 import com.app.homear.R
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,11 +22,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -57,9 +71,15 @@ fun ConfigurationScreenn()
             }
 
             //Opciones de la pantalla
-            OptionConfiguracion("NOTIFICACIONES", R.drawable.icono_toggle_on)
-            OptionConfiguracion("TEMA:CLARO", R.drawable.icono_light_mode)
-            OptionConfiguracion("IDIOMA:ESPAÑOL", R.drawable.icono_sms)
+            OptionConfiguracionCheck("NOTIFICACIONES", R.drawable.icono_toggle_on, R.drawable.icono_toggle_off, true)
+            OptionConfiguracionCheck("TEMA:CLARO", R.drawable.icono_light_mode, R.drawable.icono_light_mode, true)
+
+            //crear lista de opciones dropmenu
+            var idiomas = listOf<String>("ESPAÑOL", "INGLES", "FRANCES")
+            //IDIOMA:ESPAÑOL es el valor por defecto
+            OptionConfiguracionDropMenu("IDIOMA:ESPAÑOL", R.drawable.icono_sms, idiomas)
+
+            //botones normales
             OptionConfiguracion("COMPARTIR APP", R.drawable.icono_share)
             OptionConfiguracion("AYUDA", R.drawable.icono_error)
             OptionConfiguracion("SOBRE NOSOTROS", R.drawable.icono_group)
@@ -67,15 +87,32 @@ fun ConfigurationScreenn()
     }
 }
 
+
 /**
-    esta funcion composable define a las celdas de las opciones
-    @param nombre: define el texto que va a tener la opcion
-    @param idImagen: recibe la direccion de donde esta almacenado el icono de la opcion,
-                    dicha direccion la retorna el metodo R.drawable.nombreDelarchivo
+ * esta funcion es para celdas estaticas
+esta funcion composable define a las celdas de las opciones
+@param nombre: define el texto que va a tener la opcion
+@param idImagen: recibe la direccion de donde esta almacenado el icono de la opcion,
+dicha direccion la retorna el metodo R.drawable.nombreDelarchivo
  */
 @Composable
 fun OptionConfiguracion(nombre: String, idImagen: Int)
 {
+    // Configuración de la animación de escala
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val scale by animateFloatAsState(
+        targetValue = if (isPressed) 0.9f else 1f, // Escala 90% cuando se presiona
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioLowBouncy,
+            stiffness = Spring.StiffnessMedium
+        ),
+        label = "scaleAnimation"
+    )
+
+    Box(modifier = Modifier.scale(scale).fillMaxWidth())
+    {
+
         Row(
             modifier = Modifier
                 //el contenedor va a ocupar todo el ancho disponible
@@ -83,6 +120,13 @@ fun OptionConfiguracion(nombre: String, idImagen: Int)
                 // estilo del borde, el shape define la forma del borde
                 .border(width = 2.dp, color = Color.Gray, shape = RoundedCornerShape(15.dp))
                 .padding(10.dp) // ancho del elemento
+                .clickable(
+                    interactionSource = interactionSource,
+                    indication = null
+                )
+                {
+                    //codigo que reaacione al click
+                }
 
 
         )
@@ -90,7 +134,8 @@ fun OptionConfiguracion(nombre: String, idImagen: Int)
             // este es el contenedor de los elementos
             Box(modifier = Modifier.fillMaxWidth())
             {
-                Text(text = nombre,
+                Text(
+                    text = nombre,
                     modifier = Modifier.align(Alignment.CenterStart),
                     fontSize = 20.sp,
                 )
@@ -101,11 +146,209 @@ fun OptionConfiguracion(nombre: String, idImagen: Int)
                     modifier = Modifier.align(Alignment.CenterEnd).size(40.dp),
                     tint = Color.Magenta,
 
+                    )
+            }
+        }
+    }
+
+    //este es el margin entre los elementos
+    Spacer(modifier = Modifier.height(10.dp))
+}
+
+/**
+ *    esta funcion es para celdas tipo CheckBox
+ *     esta funcion composable define a las celdas de las opciones
+ *     @param nombre: define el texto que va a tener la opcion
+ *     @param idImagenOn: recibe la direccion de donde esta almacenado el icono de la opcion,
+ *                     dicha direccion la retorna el metodo R.drawable.nombreDelarchivo
+ *    @param idImagenOff: recibe la direccion de donde esta almacenado el icono de la opcion,
+ *                     dicha direccion la retorna el metodo R.drawable.nombreDelarchivo
+ *    @param active: define el estado inicial del objeto
+ */
+
+@Composable
+fun OptionConfiguracionCheck(nombre: String, idImagenOn: Int, idImagenOff: Int ,active: Boolean)
+{
+    var colorIcono by remember { mutableStateOf(Color.Magenta) }
+    var isActive by remember { mutableStateOf(active) }
+    var idImagen by remember { mutableStateOf(idImagenOn) }
+
+
+    // Configuración de la animación de escala
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val scale by animateFloatAsState(
+        targetValue = if (isPressed) 0.9f else 1f, // Escala 90% cuando se presiona
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioLowBouncy,
+            stiffness = Spring.StiffnessMedium
+        ),
+        label = "scaleAnimation"
+    )
+
+
+    //verifocar el estado del objeto
+    if (isActive)
+    {
+        colorIcono = Color.Magenta
+        idImagen = idImagenOn
+    }
+    else
+    {
+        colorIcono = Color.Black
+        idImagen = idImagenOff
+    }
+
+
+    Box(
+        modifier = Modifier.scale(scale).fillMaxWidth()
+    )
+    {
+
+        Row(
+            modifier = Modifier
+                //el contenedor va a ocupar todo el ancho disponible
+                .fillMaxWidth()
+                // estilo del borde, el shape define la forma del borde
+                .border(width = 2.dp, color = Color.Gray, shape = RoundedCornerShape(15.dp))
+                .padding(10.dp) // ancho del elemento
+                .clickable(
+                    interactionSource = interactionSource,
+                    indication = null
+                )
+                {
+                    if (isActive) {
+                        colorIcono = Color.Black
+                        isActive = false
+                    } else {
+                        colorIcono = Color.Magenta
+                        isActive = true
+                    }
+                }
+
+
+        )
+        {
+            // este es el contenedor de los elementos
+            Box(modifier = Modifier.fillMaxWidth())
+            {
+                Text(
+                    text = nombre,
+                    modifier = Modifier.align(Alignment.CenterStart),
+                    fontSize = 20.sp,
+                )
+                // aqui se carga el icono
+                Icon(
+                    painter = painterResource(idImagen),
+                    contentDescription = "icono Favorito",
+                    modifier = Modifier.align(Alignment.CenterEnd).size(40.dp),
+                    tint = colorIcono,
+
+                    )
+            }
+        }
+    }
+
+    //este es el margin entre los elementos
+    Spacer(modifier = Modifier.height(10.dp))
+}
+
+
+/**
+ *    esta funcion es para celdas tipo DropMenu
+ *     esta funcion composable define a las celdas de las opciones
+ *     @param nombre: define el texto que va a tener la opcion
+ *     @param idImagen: recibe la direccion de donde esta almacenado el icono de la opcion,
+ *                     dicha direccion la retorna el metodo R.drawable.nombreDelarchivo
+ *    @param opciones: son las opciones disponibles en el dropMenu
+ */
+@Composable
+fun OptionConfiguracionDropMenu(nombre: String, idImagen: Int, opciones: List<String>)
+{
+    // Configuración de la animación de escala
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val scale by animateFloatAsState(
+        targetValue = if (isPressed) 0.9f else 1f, // Escala 90% cuando se presiona
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioLowBouncy,
+            stiffness = Spring.StiffnessMedium
+        ),
+        label = "scaleAnimation"
+    )
+
+    //variable para cambiar nombre
+    var name by remember { mutableStateOf(nombre) }
+    //variables para el dropMenu
+    var expanded by remember { mutableStateOf(false) } // Estado para controlar el DropdownMenu
+    //lista de opciones para el dropMenu
+    val menuItems = opciones
+
+    Box(modifier = Modifier.scale(scale).fillMaxWidth())
+    {
+
+        Row(
+            modifier = Modifier
+                //el contenedor va a ocupar todo el ancho disponible
+                .fillMaxWidth()
+                // estilo del borde, el shape define la forma del borde
+                .border(width = 2.dp, color = Color.Gray, shape = RoundedCornerShape(15.dp))
+                .padding(10.dp) // ancho del elemento
+                .clickable(
+                    interactionSource = interactionSource,
+                    indication = null
+                )
+                {
+                    //codigo que reaacione al click
+                    expanded = true
+                }
+
+
+        )
+        {
+            // este es el contenedor de los elementos
+            Box(modifier = Modifier.fillMaxWidth())
+            {
+                Text(
+                    text = name,
+                    modifier = Modifier.align(Alignment.CenterStart),
+                    fontSize = 20.sp,
+                )
+                // aqui se carga el icono
+                Icon(
+                    painter = painterResource(idImagen),
+                    contentDescription = "icono Favorito",
+                    modifier = Modifier.align(Alignment.CenterEnd).size(40.dp),
+                    tint = Color.Magenta,
+
+                    )
+            }
+        }
+
+        //logica del dropMenu
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }, // Cerrar al tocar fuera
+            modifier = Modifier
+                .background(Color.White) // Fondo blanco para el menú
+        )
+        {
+            menuItems.forEach {
+                    item ->
+                DropdownMenuItem(
+                    text = {Text(item)},
+                    onClick = {
+                        name = "IDIOMA: $item"
+                        expanded = false
+                    }
+
                 )
             }
         }
-        //este es el margin entre los elementos
-        Spacer(modifier = Modifier.height(10.dp))
+    }
+
+    //este es el margin entre los elementos
+    Spacer(modifier = Modifier.height(10.dp))
 }
 
 
