1d3d33d64909b36ac15e1aa5abd3f218b3b3013b
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-06-14 20:29:36 -0400

    feat: intro con logo sin imagen

--- app/src/main/java/com/app/homear/ui/MainActivity.kt
+++ app/src/main/java/com/app/homear/ui/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.app.homear.ui
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.delay
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.app.homear.core.navigation.NavigationWrapper
 import com.app.homear.ui.theme.HomeARTheme
-import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.database
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.firestore
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,8 +29,47 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HomeARTheme {
-                NavigationWrapper()
+                SplashScreenWithZoom{
+                    NavigationWrapper()
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun SplashScreenWithZoom(onSplashFinished: @Composable () -> Unit) {
+    val scale = remember { Animatable(1f) }
+    val pulseTransition = rememberInfiniteTransition()
+    val pulseAnim by pulseTransition.animateFloat(
+        initialValue = 0.8f,
+        targetValue = 1.2f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1500, easing = EaseInOutCubic),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    var showContent by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        delay(2000)
+        scale.animateTo(
+            targetValue = 2f,
+            animationSpec = tween(durationMillis = 500, easing = EaseInOut)
+        )
+        showContent = true
+    }
+
+    if (showContent) {
+        onSplashFinished()
+    } else {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFF7E1B75)),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text="LOGO")
         }
     }
 }
