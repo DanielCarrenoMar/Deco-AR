2135ad38c2d61a5c4922eae0e38a4bcb4e2deb95
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2025-06-21 16:08:14 -0400

    fix: corregir error con la apikey

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,10 @@ android {
     val properties = Properties()
     properties.load(file.inputStream())
 
+    ksp {
+        arg("room.schemaLocation", "$projectDir/schemas")
+    }
+
     defaultConfig {
         applicationId = "com.app.homear"
         minSdk = 26
@@ -26,13 +30,9 @@ android {
         versionName = "1.0"
 
         val apiKey = properties.getProperty("API_GOOGLE_SERVICES") ?: ""
-        buildConfigField("String", "API_GOOGLE_SERVICES", "\"$apiKey\"")
+        buildConfigField("String", "API_GOOGLE_SERVICES", apiKey)
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        ksp {
-            arg("room.schemaLocation", "$projectDir/schemas")
-        }
     }
 
     buildTypes {
