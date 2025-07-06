85f1e2d06eb200d4718bf94baa88963d6509f87d
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-07-06 14:50:08 -0400

    feat: Descargar siempre en la misma carpeta

--- app/src/main/java/com/app/homear/domain/usecase/remoteStorage/DownloadFileFromRemoteByIdUseCase.kt
+++ app/src/main/java/com/app/homear/domain/usecase/remoteStorage/DownloadFileFromRemoteByIdUseCase.kt
@@ -1,13 +1,13 @@
 package com.app.homear.domain.usecase.remoteStorage
 
+import android.content.Context
+import android.os.Environment.DIRECTORY_PICTURES
 import android.util.Log
 import com.app.homear.domain.model.DriveFileModel
 import com.app.homear.domain.model.Resource
 import com.app.homear.domain.repository.RemoteStorageRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
-import kotlinx.coroutines.flow.flowOn
 import java.io.File
 import java.io.FileOutputStream
 import javax.inject.Inject
@@ -15,17 +15,17 @@ import javax.inject.Inject
 class DownloadFileFromRemoteByIdUseCase @Inject constructor(
     private val repository: RemoteStorageRepository
 ){
-    operator fun invoke(fileId: String, savePath: File): Flow<Resource<File>> = channelFlow {
+    operator fun invoke(fileId: String, context: Context): Flow<Resource<File>> = channelFlow {
         try {
             send(Resource.Loading())
 
             val driveFile: DriveFileModel = repository.getFileById(fileId)
                 ?: throw Exception("File not found with ID: $fileId")
 
-            Log.i("DRIVE", "Downloading file: ${driveFile.name} with ID: $fileId")
-
             val byteArray = repository.downloadFileById(fileId)
-            val file = File(savePath, driveFile.name)
+            val path = context.getExternalFilesDir(DIRECTORY_PICTURES)
+            val file = File(path, driveFile.name)
+
             FileOutputStream(file).use { it.write(byteArray) }
 
             send(Resource.Success(data = file))
